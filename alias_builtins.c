e85097b9bdb104d2dc7b6524ccea8d1b9098ddb8
Orain <112311293+okothmax@users.noreply.github.com>
2022-12-15 10:23:44 +0300

    Update alias_builtins.c
    removed whitelines

--- alias_builtins.c
+++ alias_builtins.c
@@ -1,6 +1,6 @@
 #include "shell.h"
 
-int shellby_alias(char **args, char __attribute__((__unused__)) * *front);
+int shellby_alias(char **args, char __attribute__((__unused__)) **front);
 void set_alias(char *var_name, char *value);
 void print_alias(alias_t *alias);
 
@@ -12,7 +12,7 @@ void print_alias(alias_t *alias);
  * Return: If an error occurs - -1.
  * Otherwise - 0.
  */
-int shellby_alias(char **args, char __attribute__((__unused__)) * *front)
+int shellby_alias(char **args, char __attribute__((__unused__)) **front)
 {
 	alias_t *temp = aliases;
 	int i, ret = 0;
