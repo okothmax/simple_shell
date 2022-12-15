bff8cd8fb045074085824fa1359009c2e725bd98
okothmax <okothmaxwell03@gmail.com>
2022-12-15 10:49:59 +0300

    fix

--- str_funcs2.c
+++ str_funcs2.c
@@ -5,13 +5,13 @@ int _strspn(char *s, char *accept);
 int _strcmp(char *s1, char *s2);
 int _strncmp(const char *s1, const char *s2, size_t n);
 
- /**
-  * _strchr - Locates a character in a string.
-  * @s: The string to be searched.
-  * @c: The character to be located.
-  * Return: If c is found - a pointer to the first occurence.
-  * If c is not found - NULL.
-  */
+/**
+ * *_strchr - Locates a character in a string.
+ * @s: The string to be searched.
+ * @c: The character to be located.
+ * Return: If c is found - a pointer to the first occurence.
+ * If c is not found - NULL.
+ */
 char *_strchr(char *s, char c)
 {
 	int index;
