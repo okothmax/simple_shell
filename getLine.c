58d47a5d4ac25e509d4c779bcd021c21c5fc791f
Omboya <oliviamboya1@gmail.com>
2022-12-21 12:03:18 -0800

    correction

--- getLine.c
+++ getLine.c
@@ -20,18 +20,18 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
 	*buf = NULL;
 	signal(SIGINT, sigintHandler);
 #if USE_GETLINE
-															r = getline(buf, &len_p, stdin);
+	r = getline(buf, &len_p, stdin);
 #else
-															r = _getline(info, buf, &len_p);
+	r = _getline(info, buf, &len_p);
 #endif
-															if (r > 0)
-															{
-																if ((*buf)[r - 1] == '\n')
-																{
-																	(*buf)[r - 1] = '\0'; /* remove trailing newline */
-																	r--;
-																}
-																		info->linecount_flag = 1;
+	if (r > 0)
+	{
+		if ((*buf)[r - 1] == '\n')
+		{
+			(*buf)[r - 1] = '\0'; /* remove trailing newline */
+			r--;
+		}
+		info->linecount_flag = 1;
 																		remove_comments(*buf);
 																		build_history_list(info, *buf, info->histcount++);
 																		/* if (_strchr(*buf, ';')) is this a command chain? */
